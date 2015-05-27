8a92a17a2c66e450c69c9cfd8608eed83aa81302
os-rebuild <mac@mactekiMacBook-Pro.local>
2015-05-26 17:14:32 -0700

    全局变量

---
+++ global.h
@@ -0,0 +1,30 @@
+//
+//  global.h
+//  各种测试用
+//
+//  Created by mac on 5/25/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#ifndef ______global_h
+#define ______global_h
+
+extern char card_flop0[20];
+extern char card_flop1[20];
+extern char card_flop2[20];
+extern char card_hold0[20];
+extern char card_hold1[20];
+extern char Pid[20];
+extern char Jetton[20];
+extern char Money[20];
+extern char Bet[20];
+extern char Action[20];
+extern char turn_card[20];
+extern char river_card[20];
+/*如需跟注数*/
+extern char callNum[10];
+extern char temp[7][12];
+/*明牌*/
+extern char a[5];
+extern int is_inquired;
+#endif
